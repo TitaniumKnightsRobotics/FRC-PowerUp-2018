f8e58700be8dc20fecd6ff55e5d5e16873e1e3bc
CelinaPeralta <celper19@bergen.org>
2018-02-19 11:50:48 -0500

    constant

--- src/org/usfirst/frc/team6203/robot/Constants.java
+++ src/org/usfirst/frc/team6203/robot/Constants.java
@@ -1,30 +1,29 @@
 package org.usfirst.frc.team6203.robot;
 
 public class Constants {
-	
-	//IP
-	public static final String IP = "10.62.3.52";
-	
 
-	//Drive Constants
+	// IP
+	public static final String IP = "10.62.3.52";
+
+	// Drive Constants
 	public static final double kSlow_multiplier = 0.6;
 	public static final double kDistancePerPulse = Math.PI * 4;
 	public static final double kMaxMotorOutput = 0.6;
-	
-	
-	//Chassis PID control
+
+	// Chassis PID control
 	public static final double kDriveTrainP = 1.1;
 	public static final double kDriveTrainI = 0.016;
 	public static final double kDriveTrainD = 0.8;
-	
-	//Intake
+
+	// Intake
 	public static final double kIntakeSpeed = 0.3;
 	public static final double kDepositSpeed = 0.2;
 	public static final double kIntakeP = 0.3;
 	public static final double kInSpeed = -kDepositSpeed;
-	
-	//Elevator
-	public static final double kElevatorDistancePerPulse = Math.PI * 1; //TODO
+
+	// Elevator
+	public static final double kElevatorSpeed = 0.2;
+	public static final double kElevatorDistancePerPulse = Math.PI * 1; // TODO
 	public static final double kSwitchHeight = 0;
 	public static final double kScaleHeight = 0;
 	public static final double kMaxHeight = 0;
